e73226a8dc80dbef354ee2cbe31417de3df4443a
Julia Schivinscki <Julia.Schivinscki@dellteam.com>
2025-11-17 15:30:46 -0300

    add: adicionando novas seção na sidebar e criando o componente search

--- easy-order/src/app/app.component.ts
+++ easy-order/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchComponent } from '../app/features/search/search.component'
 
 @Component({
   selector: 'eo-root',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet, 
+    SearchComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
