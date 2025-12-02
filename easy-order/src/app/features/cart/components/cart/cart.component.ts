992981679e5d900010f28cd3f9cd131aeb6e3aa9
lrquines <lrquines@sispro.com.br>
2025-12-02 13:13:52 -0300

    Ajustes nos cards

--- easy-order/src/app/features/cart/components/cart/cart.component.ts
+++ easy-order/src/app/features/cart/components/cart/cart.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
-import { PaymenteInfoComponent } from '../../../paymente-info/components/paymentInfo/paymente-info.component';
 
 @Component({
   selector: 'eo-cart',
   standalone: true,
   imports: [
-    PaymenteInfoComponent,
     CardModule
   ],
   templateUrl: './cart.component.html',
